26438c7110ba2a8a088f32c38edd88bae7de1a45
Rishbah Jain <rishabhjainfinal@gmail.com>
2021-11-06 06:53:52 +0530

    "DataAdded"

--- dev/gsap.js
+++ dev/gsap.js
@@ -129,3 +129,134 @@
         var r = e.parent || I
           , i = e.keyframes ? function _setKeyframeDefaults(i) {
             return function(t, e) {
+                for (var r in e)
+                    r in t || "duration" === r && i || "ease" === r || (t[r] = e[r])
+            }
+        }(W(e.keyframes)) : ja;
+        if (t(e.inherit))
+            for (; r; )
+                i(e, r.vars.defaults),
+                r = r.parent || r._dp;
+        return e
+    }
+    function ra(t, e, r, i) {
+        void 0 === r && (r = "_first"),
+        void 0 === i && (i = "_last");
+        var n = e._prev
+          , a = e._next;
+        n ? n._next = a : t[r] === e && (t[r] = a),
+        a ? a._prev = n : t[i] === e && (t[i] = n),
+        e._next = e._prev = e.parent = null
+    }
+    function sa(t, e) {
+        !t.parent || e && !t.parent.autoRemoveChildren || t.parent.remove(t),
+        t._act = 0
+    }
+    function ta(t, e) {
+        if (t && (!e || e._end > t._dur || e._start < 0))
+            for (var r = t; r; )
+                r._dirty = 1,
+                r = r.parent;
+        return t
+    }
+    function wa(t) {
+        return t._repeat ? gt(t._tTime, t = t.duration() + t._rDelay) * t : 0
+    }
+    function ya(t, e) {
+        return (t - e._start) * e._ts + (0 <= e._ts ? 0 : e._dirty ? e.totalDuration() : e._tDur)
+    }
+    function za(t) {
+        return t._end = da(t._start + (t._tDur / Math.abs(t._ts || t._rts || X) || 0))
+    }
+    function Aa(t, e) {
+        var r = t._dp;
+        return r && r.smoothChildTiming && t._ts && (t._start = da(r._time - (0 < t._ts ? e / t._ts : ((t._dirty ? t.totalDuration() : t._tDur) - e) / -t._ts)),
+        za(t),
+        r._dirty || ta(r, t)),
+        t
+    }
+    function Ba(t, e) {
+        var r;
+        if ((e._time || e._initted && !e._dur) && (r = ya(t.rawTime(), e),
+        (!e._dur || Tt(0, e.totalDuration(), r) - e._tTime > X) && e.render(r, !0)),
+        ta(t, e)._dp && t._initted && t._time >= t._dur && t._ts) {
+            if (t._dur < t.duration())
+                for (r = t; r._dp; )
+                    0 <= r.rawTime() && r.totalTime(r._tTime),
+                    r = r._dp;
+            t._zTime = -X
+        }
+    }
+    function Ca(t, e, r, i) {
+        return e.parent && sa(e),
+        e._start = da((q(r) ? r : r || t !== I ? bt(t, r, e) : t._time) + e._delay),
+        e._end = da(e._start + (e.totalDuration() / Math.abs(e.timeScale()) || 0)),
+        function _addLinkedListItem(t, e, r, i, n) {
+            void 0 === r && (r = "_first"),
+            void 0 === i && (i = "_last");
+            var a, s = t[i];
+            if (n)
+                for (a = e[n]; s && s[n] > a; )
+                    s = s._prev;
+            s ? (e._next = s._next,
+            s._next = e) : (e._next = t[r],
+            t[r] = e),
+            e._next ? e._next._prev = e : t[i] = e,
+            e._prev = s,
+            e.parent = e._dp = t
+        }(t, e, "_first", "_last", t._sort ? "_start" : 0),
+        vt(e) || (t._recent = e),
+        i || Ba(t, e),
+        t
+    }
+    function Da(t, e) {
+        return (ot.ScrollTrigger || N("scrollTrigger", e)) && ot.ScrollTrigger.create(e, t)
+    }
+    function Ea(t, e, r, i) {
+        return jt(t, e),
+        t._initted ? !r && t._pt && (t._dur && !1 !== t.vars.lazy || !t._dur && t.vars.lazy) && f !== St.frame ? (ht.push(t),
+        t._lazy = [e, i],
+        1) : void 0 : 1
+    }
+    function Ja(t, e, r, i) {
+        var n = t._repeat
+          , a = da(e) || 0
+          , s = t._tTime / t._tDur;
+        return s && !i && (t._time *= a / t._dur),
+        t._dur = a,
+        t._tDur = n ? n < 0 ? 1e10 : da(a * (n + 1) + t._rDelay * n) : a,
+        0 < s && !i ? Aa(t, t._tTime = t._tDur * s) : t.parent && za(t),
+        r || ta(t.parent, t),
+        t
+    }
+    function Ka(t) {
+        return t instanceof Nt ? ta(t) : Ja(t, t._dur)
+    }
+    function Na(e, r, i) {
+        var n, a, s = q(r[1]), o = (s ? 2 : 1) + (e < 2 ? 0 : 1), u = r[o];
+        if (s && (u.duration = r[1]),
+        u.parent = i,
+        e) {
+            for (n = u,
+            a = i; a && !("immediateRender"in n); )
+                n = a.vars.defaults || {},
+                a = t(a.vars.inherit) && a.parent;
+            u.immediateRender = t(n.immediateRender),
+            e < 2 ? u.runBackwards = 1 : u.startAt = r[o - 1]
+        }
+        return new Jt(r[0],u,r[1 + o])
+    }
+    function Oa(t, e) {
+        return t || 0 === t ? e(t) : e
+    }
+    function Qa(t, e) {
+        return o(t) && (e = st.exec(t)) ? t.substr(e.index + e[0].length) : ""
+    }
+    function Ta(t, e) {
+        return t && s(t) && "length"in t && (!e && !t.length || t.length - 1 in t && s(t[0])) && !t.nodeType && t !== i
+    }
+    function Xa(t) {
+        return t.sort(function() {
+            return .5 - Math.random()
+        })
+    }
