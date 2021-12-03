f70f560cd055911b9fd472ccb67aec5b9c7cf7f8
Rishbah Jain <rishabhjainfinal@gmail.com>
2021-12-03 06:54:03 +0530

    "DataAdded"

--- stage1/gsap.js
+++ stage1/gsap.js
@@ -44,3 +44,134 @@
     function v(t) {
         return p(t) || o(t)
     }
+    function M(t) {
+        return (h = mt(t, ot)) && oe
+    }
+    function N(t, e) {
+        return console.warn("Invalid property", t, "set to", e, "Missing plugin? gsap.registerPlugin()")
+    }
+    function O(t, e) {
+        return !e && console.warn(t)
+    }
+    function P(t, e) {
+        return t && (ot[t] = e) && h && (h[t] = e) || ot
+    }
+    function Q() {
+        return 0
+    }
+    function $(t) {
+        var e, r, i = t[0];
+        if (s(i) || p(i) || (t = [t]),
+        !(e = (i._gsap || {}).harness)) {
+            for (r = ct.length; r-- && !ct[r].targetTest(i); )
+                ;
+            e = ct[r]
+        }
+        for (r = t.length; r--; )
+            t[r] && (t[r]._gsap || (t[r]._gsap = new Lt(t[r],e))) || t.splice(r, 1);
+        return t
+    }
+    function _(t) {
+        return t._gsap || $(xt(t))[0]._gsap
+    }
+    function aa(t, e, i) {
+        return (i = t[e]) && p(i) ? t[e]() : r(i) && t.getAttribute && t.getAttribute(e) || i
+    }
+    function ba(t, e) {
+        return (t = t.split(",")).forEach(e) || t
+    }
+    function ca(t) {
+        return Math.round(1e5 * t) / 1e5 || 0
+    }
+    function da(t) {
+        return Math.round(1e7 * t) / 1e7 || 0
+    }
+    function ea(t, e) {
+        for (var r = e.length, i = 0; t.indexOf(e[i]) < 0 && ++i < r; )
+            ;
+        return i < r
+    }
+    function fa() {
+        var t, e, r = ht.length, i = ht.slice(0);
+        for (lt = {},
+        t = ht.length = 0; t < r; t++)
+            (e = i[t]) && e._lazy && (e.render(e._lazy[0], e._lazy[1], !0)._lazy = 0)
+    }
+    function ga(t, e, r, i) {
+        ht.length && fa(),
+        t.render(e, r, i),
+        ht.length && fa()
+    }
+    function ha(t) {
+        var e = parseFloat(t);
+        return (e || 0 === e) && (t + "").match(at).length < 2 ? e : o(t) ? t.trim() : t
+    }
+    function ia(t) {
+        return t
+    }
+    function ja(t, e) {
+        for (var r in e)
+            r in t || (t[r] = e[r]);
+        return t
+    }
+    function ma(t, e) {
+        for (var r in e)
+            "__proto__" !== r && "constructor" !== r && "prototype" !== r && (t[r] = s(e[r]) ? ma(t[r] || (t[r] = {}), e[r]) : e[r]);
+        return t
+    }
+    function na(t, e) {
+        var r, i = {};
+        for (r in t)
+            r in e || (i[r] = t[r]);
+        return i
+    }
+    function oa(e) {
+        var r = e.parent || I
+          , i = e.keyframes ? function _setKeyframeDefaults(i) {
+            return function(t, e) {
+                for (var r in e)
+                    r in t || "duration" === r && i || "ease" === r || (t[r] = e[r])
+            }
+        }(W(e.keyframes)) : ja;
+        if (t(e.inherit))
+            for (; r; )
+                i(e, r.vars.defaults),
+                r = r.parent || r._dp;
+        return e
+    }
+    function ra(t, e, r, i) {
+        void 0 === r && (r = "_first"),
+        void 0 === i && (i = "_last");
+        var n = e._prev
+          , a = e._next;
+        n ? n._next = a : t[r] === e && (t[r] = a),
+        a ? a._prev = n : t[i] === e && (t[i] = n),
+        e._next = e._prev = e.parent = null
+    }
+    function sa(t, e) {
+        !t.parent || e && !t.parent.autoRemoveChildren || t.parent.remove(t),
+        t._act = 0
+    }
+    function ta(t, e) {
+        if (t && (!e || e._end > t._dur || e._start < 0))
+            for (var r = t; r; )
+                r._dirty = 1,
+                r = r.parent;
+        return t
+    }
+    function wa(t) {
+        return t._repeat ? gt(t._tTime, t = t.duration() + t._rDelay) * t : 0
+    }
+    function ya(t, e) {
+        return (t - e._start) * e._ts + (0 <= e._ts ? 0 : e._dirty ? e.totalDuration() : e._tDur)
+    }
+    function za(t) {
+        return t._end = da(t._start + (t._tDur / Math.abs(t._ts || t._rts || X) || 0))
+    }
+    function Aa(t, e) {
+        var r = t._dp;
+        return r && r.smoothChildTiming && t._ts && (t._start = da(r._time - (0 < t._ts ? e / t._ts : ((t._dirty ? t.totalDuration() : t._tDur) - e) / -t._ts)),
+        za(t),
+        r._dirty || ta(r, t)),
+        t
+    }
