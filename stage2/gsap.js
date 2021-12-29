f63c2764daa886afa67a2a60bf9e8c5f5a0994e2
Rishbah Jain <rishabhjainfinal@gmail.com>
2021-12-29 06:54:13 +0530

    "DataAdded"

---
+++ stage2/gsap.js
@@ -0,0 +1,58 @@
+/*!
+ * GSAP 3.9.1
+ * https://greensock.com
+ * 
+ * @license Copyright 2021, GreenSock. All rights reserved.
+ * Subject to the terms at https://greensock.com/standard-license or for Club GreenSock members, the agreement issued with that membership.
+ * @author: Jack Doyle, jack@greensock.com
+ */
+
+!function(t, e) {
+    "object" == typeof exports && "undefined" != typeof module ? e(exports) : "function" == typeof define && define.amd ? define(["exports"], e) : e((t = t || self).window = t.window || {})
+}(this, function(e) {
+    "use strict";
+    function _inheritsLoose(t, e) {
+        t.prototype = Object.create(e.prototype),
+        (t.prototype.constructor = t).__proto__ = e
+    }
+    function _assertThisInitialized(t) {
+        if (void 0 === t)
+            throw new ReferenceError("this hasn't been initialised - super() hasn't been called");
+        return t
+    }
+    function o(t) {
+        return "string" == typeof t
+    }
+    function p(t) {
+        return "function" == typeof t
+    }
+    function q(t) {
+        return "number" == typeof t
+    }
+    function r(t) {
+        return void 0 === t
+    }
+    function s(t) {
+        return "object" == typeof t
+    }
+    function t(t) {
+        return !1 !== t
+    }
+    function u() {
+        return "undefined" != typeof window
+    }
+    function v(t) {
+        return p(t) || o(t)
+    }
+    function M(t) {
+        return (h = mt(t, ot)) && oe
+    }
+    function N(t, e) {
+        return console.warn("Invalid property", t, "set to", e, "Missing plugin? gsap.registerPlugin()")
+    }
+    function O(t, e) {
+        return !e && console.warn(t)
+    }
+    function P(t, e) {
+        return t && (ot[t] = e) && h && (h[t] = e) || ot
+    }
