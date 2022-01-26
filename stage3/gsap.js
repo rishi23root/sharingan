225af9511911179f309a789cf4b894a915d7b0a1
Rishbah Jain <rishabhjainfinal@gmail.com>
2022-01-26 06:54:22 +0530

    "DataAdded"

---
+++ stage3/gsap.js
@@ -0,0 +1,104 @@
+/*!
+ * GSAP 3.9.1
+ * https://greensock.com
+ * 
+ * @license Copyright 2021, GreenSock. All rights reserved.
+ * Subject to the terms at https://greensock.com/standard-license or for Club GreenSock members, the agreement issued with that membership.
+ * @author: Jack Doyle, jack@greensock.com
+ */
+
+!function(t, e) {
+    "object" == typeof exports && "undefined" != typeof module ? e(exports) : "function" == typeof define && define.amd ? define(["exports"], e) : e((t = t || self).window = t.window || {})
+}(this, function(e) {
+    "use strict";
+    function _inheritsLoose(t, e) {
+        t.prototype = Object.create(e.prototype),
+        (t.prototype.constructor = t).__proto__ = e
+    }
+    function _assertThisInitialized(t) {
+        if (void 0 === t)
+            throw new ReferenceError("this hasn't been initialised - super() hasn't been called");
+        return t
+    }
+    function o(t) {
+        return "string" == typeof t
+    }
+    function p(t) {
+        return "function" == typeof t
+    }
+    function q(t) {
+        return "number" == typeof t
+    }
+    function r(t) {
+        return void 0 === t
+    }
+    function s(t) {
+        return "object" == typeof t
+    }
+    function t(t) {
+        return !1 !== t
+    }
+    function u() {
+        return "undefined" != typeof window
+    }
+    function v(t) {
+        return p(t) || o(t)
+    }
+    function M(t) {
+        return (h = mt(t, ot)) && oe
+    }
+    function N(t, e) {
+        return console.warn("Invalid property", t, "set to", e, "Missing plugin? gsap.registerPlugin()")
+    }
+    function O(t, e) {
+        return !e && console.warn(t)
+    }
+    function P(t, e) {
+        return t && (ot[t] = e) && h && (h[t] = e) || ot
+    }
+    function Q() {
+        return 0
+    }
+    function $(t) {
+        var e, r, i = t[0];
+        if (s(i) || p(i) || (t = [t]),
+        !(e = (i._gsap || {}).harness)) {
+            for (r = ct.length; r-- && !ct[r].targetTest(i); )
+                ;
+            e = ct[r]
+        }
+        for (r = t.length; r--; )
+            t[r] && (t[r]._gsap || (t[r]._gsap = new Lt(t[r],e))) || t.splice(r, 1);
+        return t
+    }
+    function _(t) {
+        return t._gsap || $(xt(t))[0]._gsap
+    }
+    function aa(t, e, i) {
+        return (i = t[e]) && p(i) ? t[e]() : r(i) && t.getAttribute && t.getAttribute(e) || i
+    }
+    function ba(t, e) {
+        return (t = t.split(",")).forEach(e) || t
+    }
+    function ca(t) {
+        return Math.round(1e5 * t) / 1e5 || 0
+    }
+    function da(t) {
+        return Math.round(1e7 * t) / 1e7 || 0
+    }
+    function ea(t, e) {
+        for (var r = e.length, i = 0; t.indexOf(e[i]) < 0 && ++i < r; )
+            ;
+        return i < r
+    }
+    function fa() {
+        var t, e, r = ht.length, i = ht.slice(0);
+        for (lt = {},
+        t = ht.length = 0; t < r; t++)
+            (e = i[t]) && e._lazy && (e.render(e._lazy[0], e._lazy[1], !0)._lazy = 0)
+    }
+    function ga(t, e, r, i) {
+        ht.length && fa(),
+        t.render(e, r, i),
+        ht.length && fa()
+    }
