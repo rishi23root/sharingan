c3601b7ae0cc2ec5e252be81b37cb5cc136e8ceb
Rishbah Jain <rishabhjainfinal@gmail.com>
2022-02-22 06:54:30 +0530

    "DataAdded"

--- stage3/MorphSVGPlugin.js
+++ stage3/MorphSVGPlugin.js
@@ -441,3 +441,31 @@
             },
             stringToRawPath: stringToRawPath,
             rawPathToString: rawPathToString,
+            normalizeStrings: function normalizeStrings(t, e, n) {
+                var r = n.shapeIndex,
+                    o = n.map,
+                    i = [t, e];
+                return ca(i, r, o), i
+            },
+            pathFilter: ca,
+            pointsFilter: fa,
+            getTotalSize: U,
+            equalizeSegmentQuantity: ba,
+            convertToPath: function convertToPath$1(t, e) {
+                return j(t).map(function (t) {
+                    return convertToPath(t, !1 !== e)
+                })
+            },
+            defaultType: "linear",
+            defaultUpdateTarget: !0,
+            defaultMap: "size"
+        };
+    y() && o.registerPlugin(K), t.MorphSVGPlugin = K, t.default = K;
+    if (typeof (window) === "undefined" || window !== t) {
+        Object.defineProperty(t, "__esModule", {
+            value: !0
+        })
+    } else {
+        delete t.default
+    }
+});
---
+++ stage4/gsap.js
@@ -0,0 +1,15 @@
+/*!
+ * GSAP 3.9.1
+ * https://greensock.com
+ * 
+ * @license Copyright 2021, GreenSock. All rights reserved.
+ * Subject to the terms at https://greensock.com/standard-license or for Club GreenSock members, the agreement issued with that membership.
+ * @author: Jack Doyle, jack@greensock.com
+ */
+
+!function(t, e) {
+    "object" == typeof exports && "undefined" != typeof module ? e(exports) : "function" == typeof define && define.amd ? define(["exports"], e) : e((t = t || self).window = t.window || {})
+}(this, function(e) {
+    "use strict";
+    function _inheritsLoose(t, e) {
+        t.prototype = Object.create(e.prototype),
