dec6a2e7c5bb938d8ac2f21937ef36412de2416d
Rishbah Jain <rishabhjainfinal@gmail.com>
2022-02-23 06:54:31 +0530

    "DataAdded"

--- stage4/gsap.js
+++ stage4/gsap.js
@@ -13,3 +13,134 @@
     "use strict";
     function _inheritsLoose(t, e) {
         t.prototype = Object.create(e.prototype),
+        (t.prototype.constructor = t).__proto__ = e
+    }
+    function _assertThisInitialized(t) {
+        if (void 0 === t)
+            throw new ReferenceError("this hasn't been initialised - super() hasn't been called");
+        return t
+    }
+    function o(t) {
+        return "string" == typeof t
+    }
+    function p(t) {
+        return "function" == typeof t
+    }
+    function q(t) {
+        return "number" == typeof t
+    }
+    function r(t) {
+        return void 0 === t
+    }
+    function s(t) {
+        return "object" == typeof t
+    }
+    function t(t) {
+        return !1 !== t
+    }
+    function u() {
+        return "undefined" != typeof window
+    }
+    function v(t) {
+        return p(t) || o(t)
+    }
+    function M(t) {
+        return (h = mt(t, ot)) && oe
+    }
+    function N(t, e) {
+        return console.warn("Invalid property", t, "set to", e, "Missing plugin? gsap.registerPlugin()")
+    }
+    function O(t, e) {
+        return !e && console.warn(t)
+    }
+    function P(t, e) {
+        return t && (ot[t] = e) && h && (h[t] = e) || ot
+    }
+    function Q() {
+        return 0
+    }
+    function $(t) {
+        var e, r, i = t[0];
+        if (s(i) || p(i) || (t = [t]),
+        !(e = (i._gsap || {}).harness)) {
+            for (r = ct.length; r-- && !ct[r].targetTest(i); )
+                ;
+            e = ct[r]
+        }
+        for (r = t.length; r--; )
+            t[r] && (t[r]._gsap || (t[r]._gsap = new Lt(t[r],e))) || t.splice(r, 1);
+        return t
+    }
+    function _(t) {
+        return t._gsap || $(xt(t))[0]._gsap
+    }
+    function aa(t, e, i) {
+        return (i = t[e]) && p(i) ? t[e]() : r(i) && t.getAttribute && t.getAttribute(e) || i
+    }
+    function ba(t, e) {
+        return (t = t.split(",")).forEach(e) || t
+    }
+    function ca(t) {
+        return Math.round(1e5 * t) / 1e5 || 0
+    }
+    function da(t) {
+        return Math.round(1e7 * t) / 1e7 || 0
+    }
+    function ea(t, e) {
+        for (var r = e.length, i = 0; t.indexOf(e[i]) < 0 && ++i < r; )
+            ;
+        return i < r
+    }
+    function fa() {
+        var t, e, r = ht.length, i = ht.slice(0);
+        for (lt = {},
+        t = ht.length = 0; t < r; t++)
+            (e = i[t]) && e._lazy && (e.render(e._lazy[0], e._lazy[1], !0)._lazy = 0)
+    }
+    function ga(t, e, r, i) {
+        ht.length && fa(),
+        t.render(e, r, i),
+        ht.length && fa()
+    }
+    function ha(t) {
+        var e = parseFloat(t);
+        return (e || 0 === e) && (t + "").match(at).length < 2 ? e : o(t) ? t.trim() : t
+    }
+    function ia(t) {
+        return t
+    }
+    function ja(t, e) {
+        for (var r in e)
+            r in t || (t[r] = e[r]);
+        return t
+    }
+    function ma(t, e) {
+        for (var r in e)
+            "__proto__" !== r && "constructor" !== r && "prototype" !== r && (t[r] = s(e[r]) ? ma(t[r] || (t[r] = {}), e[r]) : e[r]);
+        return t
+    }
+    function na(t, e) {
+        var r, i = {};
+        for (r in t)
+            r in e || (i[r] = t[r]);
+        return i
+    }
+    function oa(e) {
+        var r = e.parent || I
+          , i = e.keyframes ? function _setKeyframeDefaults(i) {
+            return function(t, e) {
+                for (var r in e)
+                    r in t || "duration" === r && i || "ease" === r || (t[r] = e[r])
+            }
+        }(W(e.keyframes)) : ja;
+        if (t(e.inherit))
+            for (; r; )
+                i(e, r.vars.defaults),
+                r = r.parent || r._dp;
+        return e
+    }
+    function ra(t, e, r, i) {
+        void 0 === r && (r = "_first"),
+        void 0 === i && (i = "_last");
+        var n = e._prev
+          , a = e._next;
